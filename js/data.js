3e726d854d4681a639d539a57b201607778d03c3
aditeyapandey <aditeyapandey@gmail.com>
2022-03-11 20:54:11 -0500

    add connection information to all files

--- js/data.js
+++ js/data.js
@@ -8,6 +8,7 @@ window.data = {
           attributes: "1 Quantitative",
           featureExtent: "Point",
           featureDensity: "Sparse",
+          connection: "No",
         },
       ],
       taskDescription:
@@ -15,11 +16,11 @@ window.data = {
       stimuli: [
         {
           output: "genorec",
-          spec: "1_g_spec.json"
+          spec: "1_g_spec.json",
         },
         {
           output: "random",
-          spec: "1_a_spec.json"
+          spec: "1_a_spec.json",
         },
       ],
     },
@@ -27,22 +28,29 @@ window.data = {
       id: 2,
       dataDescription: [
         {
-          fileFormat: "BIGWIG",
-          attributes: "1 Quantitative",
+          fileFormat: "VCF",
+          attributes: "1 Categorical",
           featureExtent: "Point",
-          featureDensity: "Contiguous",
+          featureDensity: "Sparse",
+          connection: "No",
+        },
+        {
+          fileFormat: "BED",
+          attributes: "1 Text",
+          featureExtent: "Segment",
+          featureDensity: "Sparse",
+          connection: "No",
         },
       ],
-      taskDescription:
-        "Analyze inter-chromosomal connections.",
+      taskDescription: "Find the most common category in Chr 3.",
       stimuli: [
         {
           output: "genorec",
-          spec: "1_g_spec.json"
+          spec: "1_g_spec.json",
         },
         {
           output: "random",
-          spec: "1_a_spec.json"
+          spec: "1_a_spec.json",
         },
       ],
     },
@@ -54,18 +62,18 @@ window.data = {
           attributes: "N.A.",
           featureExtent: "Segment",
           featureDensity: "Sparse",
+          connection: "Yes",
         },
       ],
-      taskDescription:
-        "Analyze inter-chromosomal connections.",
+      taskDescription: "Analyze inter-chromosomal connections.",
       stimuli: [
         {
           output: "genorec",
-          spec: "3_g_spec.json"
+          spec: "3_g_spec.json",
         },
         {
           output: "random",
-          spec: "3_a_spec.json"
+          spec: "3_a_spec.json",
         },
       ],
     },
@@ -77,18 +85,18 @@ window.data = {
           attributes: "1 Quantitative",
           featureExtent: "Point",
           featureDensity: "Contiguous",
+          connection: "No",
         },
       ],
-      taskDescription:
-        "Identify patterns in the genome.",
+      taskDescription: "Identify patterns in the genome.",
       stimuli: [
         {
           output: "genorec",
-          spec: "4_g_spec.json"
+          spec: "4_g_spec.json",
         },
         {
           output: "random",
-          spec: "4_a_spec.json"
+          spec: "4_a_spec.json",
         },
       ],
     },
@@ -100,18 +108,18 @@ window.data = {
           attributes: "1 Text",
           featureExtent: "Segment",
           featureDensity: "Sparse",
-        }
+          connection: "No",
+        },
       ],
-      taskDescription:
-        "Find the location of a specific gene.",
+      taskDescription: "Find the location of a specific gene.",
       stimuli: [
         {
           output: "genorec",
-          spec: "5_g_spec.json"
+          spec: "5_g_spec.json",
         },
         {
           output: "random",
-          spec: "5_a_spec.json"
+          spec: "5_a_spec.json",
         },
       ],
     },
@@ -123,24 +131,25 @@ window.data = {
           attributes: "1 Quantitative",
           featureExtent: "Point",
           featureDensity: "Contiguous",
+          connection: "No",
         },
         {
           fileFormat: "BED",
           attributes: "1 Categorical",
           featureExtent: "Segment",
           featureDensity: "Sparse",
-        }
+          connection: "No",
+        },
       ],
-      taskDescription:
-        "Analyze values in a specific region of the genome.",
+      taskDescription: "Analyze values in a specific region of the genome.",
       stimuli: [
         {
           output: "genorec",
-          spec: "6_g_spec.json"
+          spec: "6_g_spec.json",
         },
         {
           output: "random",
-          spec: "6_a_spec.json"
+          spec: "6_a_spec.json",
         },
       ],
     },
@@ -152,24 +161,25 @@ window.data = {
           attributes: "1 Quantitative",
           featureExtent: "Point",
           featureDensity: "Contiguous",
+          connection: "No",
         },
         {
           fileFormat: "BIGWIG",
           attributes: "1 Quantitative",
           featureExtent: "Point",
           featureDensity: "Contiguous",
-        }
+          connection: "No",
+        },
       ],
-      taskDescription:
-        "Compare values between two regions of the genome.",
+      taskDescription: "Compare values between two regions of the genome.",
       stimuli: [
         {
           output: "genorec",
-          spec: "7_g_spec.json"
+          spec: "7_g_spec.json",
         },
         {
           output: "random",
-          spec: "7_a_spec.json"
+          spec: "7_a_spec.json",
         },
       ],
     },
@@ -181,18 +191,18 @@ window.data = {
           attributes: "1 Quantitative",
           featureExtent: "Point",
           featureDensity: "Contiguous",
-        }
+          connection: "No",
+        },
       ],
-      taskDescription:
-        "Identify the highest value in chromosome 3.",
+      taskDescription: "Identify the highest value in chromosome 3.",
       stimuli: [
         {
           output: "genorec",
-          spec: "8_g_spec.json"
+          spec: "8_g_spec.json",
         },
         {
           output: "random",
-          spec: "8_a_spec.json"
+          spec: "8_a_spec.json",
         },
       ],
     },
@@ -204,24 +214,25 @@ window.data = {
           attributes: "1 Categorical",
           featureExtent: "Segment",
           featureDensity: "Sparse",
+          connection: "No",
         },
         {
           fileFormat: "BED",
           attributes: "1 Categorical",
           featureExtent: "Segment",
           featureDensity: "Sparse",
-        }
+          connection: "No",
+        },
       ],
-      taskDescription:
-        "Find regions which contain similar categorical data",
+      taskDescription: "Find regions which contain similar categorical data",
       stimuli: [
         {
           output: "genorec",
-          spec: "9_g_spec.json"
+          spec: "9_g_spec.json",
         },
         {
           output: "random",
-          spec: "9_a_spec.json"
+          spec: "9_a_spec.json",
         },
       ],
     },
@@ -233,24 +244,26 @@ window.data = {
           attributes: "1 Quantitative",
           featureExtent: "Point",
           featureDensity: "Contiguous",
+          connection: "No",
         },
         {
           fileFormat: "SEG",
           attributes: "1 Quantitative",
           featureExtent: "Segment",
           featureDensity: "Sparse",
-        }
+          connection: "No",
+        },
       ],
       taskDescription:
         "Find the areas in the genome where both quantitative attributes have high values.",
       stimuli: [
         {
           output: "genorec",
-          spec: "10_g_spec.json"
+          spec: "10_g_spec.json",
         },
         {
           output: "random",
-          spec: "10_a_spec.json"
+          spec: "10_a_spec.json",
         },
       ],
     },
@@ -262,30 +275,33 @@ window.data = {
           attributes: "1 Categorical",
           featureExtent: "Segment",
           featureDensity: "Sparse",
+          connection: "No",
         },
         {
           fileFormat: "BED",
           attributes: "1 Quantitative",
           featureExtent: "Segment",
           featureDensity: "Sparse",
+          connection: "No",
         },
         {
           fileFormat: "BED",
           attributes: "1 Text",
           featureExtent: "Segment",
           featureDensity: "Sparse",
-        }
+          connection: "No",
+        },
       ],
       taskDescription:
         "Read categorical, quantitative and text values for a specific location.",
       stimuli: [
         {
           output: "genorec",
-          spec: "11_g_spec.json"
+          spec: "11_g_spec.json",
         },
         {
           output: "random",
-          spec: "11_a_spec.json"
+          spec: "11_a_spec.json",
         },
       ],
     },
@@ -297,18 +313,18 @@ window.data = {
           attributes: "1 Categorical, 1 Text",
           featureExtent: "Segment",
           featureDensity: "Sparse",
-        }
+          connection: "No",
+        },
       ],
-      taskDescription:
-        "Look for a specific gene in the genome",
+      taskDescription: "Look for a specific gene in the genome",
       stimuli: [
         {
           output: "genorec",
-          spec: "12_g_spec.json"
+          spec: "12_g_spec.json",
         },
         {
           output: "random",
-          spec: "12_a_spec.json"
+          spec: "12_a_spec.json",
         },
       ],
     },
@@ -320,13 +336,14 @@ window.data = {
           attributes: "1 Categorical",
           featureExtent: "Segment",
           featureDensity: "Sparse",
-          connection: "Yes"
+          connection: "Yes",
         },
         {
           fileFormat: "VCF",
           attributes: "1 Quantitative",
           featureExtent: "Point",
           featureDensity: "Sparse",
+          connection: "No",
         },
       ],
       taskDescription:
@@ -334,11 +351,11 @@ window.data = {
       stimuli: [
         {
           output: "genorec",
-          spec: "13_g_spec.json"
+          spec: "13_g_spec.json",
         },
         {
           output: "random",
-          spec: "13_a_spec.json"
+          spec: "13_a_spec.json",
         },
       ],
     },
@@ -350,30 +367,32 @@ window.data = {
           attributes: "N.A.",
           featureExtent: "Segment",
           featureDensity: "Contiguous",
+          connection: "Yes"
         },
         {
           fileFormat: "BIGWIG",
           attributes: "1 Quantitative",
           featureExtent: "Point",
           featureDensity: "Contiguous",
+          connection: "No"
         },
         {
           fileFormat: "BED",
           attributes: "1 Quantitative",
           featureExtent: "Segment",
           featureDensity: "Sparse",
-        }
+          connection: "No"
+        },
       ],
-      taskDescription:
-        "Identify areas of high connectivity.",
+      taskDescription: "Identify areas of high connectivity.",
       stimuli: [
         {
           output: "genorec",
-          spec: "14_g_spec.json"
+          spec: "14_g_spec.json",
         },
         {
           output: "random",
-          spec: "14_a_spec.json"
+          spec: "14_a_spec.json",
         },
       ],
     },
